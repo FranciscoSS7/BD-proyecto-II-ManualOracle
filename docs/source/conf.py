724779f876f042a5f639d4fd072076d39409376c
FranciscoSS7 <FranciscoSS7@users.noreply.github.com>
2016-06-11 17:34:36 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'Home'
+master_doc = 'index'
 
 # General information about the project.
 project = 'Bases de Datos I | Proyecto II'
